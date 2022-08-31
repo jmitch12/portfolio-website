237e6c8b89b01d77b302cac441ee027446c86858
Jane Mitchell <mitcj785@newschool.edu>
2022-08-31 18:05:44 -0500

    Updates based on user and mentor feedback
    Updated menu styling, changed page margins for consistency, fixed mobile footer, rearranged css, 1.4 changes pushed to GitHub, change hamburger to “x” when menu open, validated code, added scroll reveal

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-console.log("Hi my name is Jane, welcome to my portfolio site!");
+console.log('Hi my name is Jane, welcome to my portfolio site!')
 
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
+
+let changeIcon = function (icon){
+  icon.classList.toggle('fa-times')
+}
+
+ScrollReveal().reveal('.section');
